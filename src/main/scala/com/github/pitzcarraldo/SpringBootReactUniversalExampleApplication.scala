2c29acb8330e7d7fb2c1400c9f0041a5c520688d
Minkyu Cho <mrnoname@naver.com>
2017-01-18 03:54:31 +0900

    Apply github repository

--- src/main/scala/com/github/pitzcarraldo/SpringBootReactUniversalExampleApplication.scala
+++ src/main/scala/com/github/pitzcarraldo/SpringBootReactUniversalExampleApplication.scala
@@ -1,36 +1,45 @@
 package com.github.pitzcarraldo
 
+import java.nio.charset.Charset
+import javax.servlet.Filter
+
 import com.github.pitzcarraldo.spring.view.node.{NodeView, NodeViewResolver}
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.{HttpMessageConverter, StringHttpMessageConverter}
+import org.springframework.web.filter.CharacterEncodingFilter
 import org.springframework.web.servlet.ViewResolver
-import org.springframework.web.servlet.config.annotation.{DefaultServletHandlerConfigurer, EnableWebMvc, WebMvcConfigurerAdapter}
 
 /**
   * @author Minkyu Cho (https://github.com/Pitzcarraldo)
   */
 @SpringBootApplication
 class SpringBootReactUniversalExampleApplication
-object SpringBootReactUniversalExampleApplication extends WebMvcConfigurerAdapter {
+object SpringBootReactUniversalExampleApplication {
   def main(args: Array[String]) {
     SpringApplication.run(classOf[SpringBootReactUniversalExampleApplication], args: _*)
   }
-}
 
-@Configuration
-@EnableWebMvc
-class MvcConfig extends WebMvcConfigurerAdapter {
+  @Bean
+  def responseBodyConverter(): HttpMessageConverter[String] = new StringHttpMessageConverter(Charset.forName("UTF-8"))
+
+  @Bean
+  def characterEncodingFilter(): Filter = {
+    val characterEncodingFilter = new CharacterEncodingFilter
+    characterEncodingFilter.setEncoding("UTF-8")
+    characterEncodingFilter.setForceEncoding(true)
+    characterEncodingFilter
+  }
+
   @Bean
   def getViewResolver: ViewResolver = {
     val resolver = new NodeViewResolver
     resolver.setPrefix("/WEB-INF/views/")
     resolver.setSuffix(".js")
     resolver.setViewClass(classOf[NodeView])
+    resolver.setContentType("text/html;charset=UTF-8")
     resolver.setCache(false)
     resolver
   }
-
-  override def configureDefaultServletHandling(configurer: DefaultServletHandlerConfigurer): Unit =
-    configurer.enable()
 }
--- src/main/webapp/WEB-INF/views/hello.js
+++ src/main/webapp/WEB-INF/views/hello.js
@@ -1,3 +1,3 @@
 module.exports = function render(model, callback) {
-  callback("test")
+    callback("hello")
 };
